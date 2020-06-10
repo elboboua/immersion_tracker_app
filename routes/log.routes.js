6aa7d6f442b4db36814680a2d735d611dae23ad7
elboboua <elboboua@oregonstate.edu>
2020-06-11 00:05:38 +0300

    fixed date problem by converting the date to just YYYY-MM-DD format. I think there was an issue between how knex/js and mysql handle dates.

--- routes/log.routes.js
+++ routes/log.routes.js
@@ -37,16 +37,22 @@ router.post('/delete', async (req, res) => {
 })
 
 router.post('/getLastWeek', async (req, res) => {
+
+    console.log(req.body)
     let result = await knex('log')
-    .select('date')
-    .sum('time as time',)
-    .groupBy('date')
-    .as('time')
+    .select(knex.raw('date_format(date, "%Y-%m-%d") as date, sum(time) as time'))
     .where({user_id: req.user.id})
     .andWhere('date', '<=', req.body.today)
     .andWhere('date', '>=', req.body.lastWeek)
+    .groupBy('date')
     .orderBy('date', 'asc');
     console.log(result)
+
+    let result2  = await knex.raw(
+        //`select sum(time), date from log where date between ${req.body.lastWeek} and ${req.body.today} and user_id = ${req.user.id} group by date`
+        "select date, sum(time) from log where date between '2020-06-02' and '2020-06-09' and user_id = 9 group by date"
+        )
+        console.log(result2)
 
     res.send(result);
 })
