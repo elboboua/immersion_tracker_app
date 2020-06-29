8710e477296e307d621a42750d0a8aebed7b5731
elboboua <elboboua@oregonstate.edu>
2020-06-29 19:46:24 +0300

    refactorsed the log route by creating some log models

---
+++ models/log.models.js
@@ -0,0 +1,93 @@
+const knex = require('../config/KnexConnection');
+
+const getAllLogsByID = (id) => {
+    return knex('log')
+    .where({user_id: id, deleted: false})
+    .orderBy('date_created', 'desc')
+    .orderBy('id', 'desc');
+}
+
+const getLogsDateByID = (id) => {
+    return knex('log')
+    .select(knex.raw("distinct(DATE_FORMAT(date_created, '%Y-%m-%d')) as date"))
+    .where({user_id: id, deleted: false})
+    .orderBy('date', 'desc');
+}
+
+const insertLog = (rb, ru) => {
+    console.log(rb)
+    let log = {
+        name: rb.name,
+        user_id: parseInt(ru.id),
+        language_id: parseInt(rb.language_id),
+        type_id: parseInt(rb.type_id),
+        time: rb.time,
+        date: rb.date,
+        date_created: new Date().toISOString().slice(0,10),
+    };
+    return knex('log').insert(log);
+}
+
+const deleteLog = (rb) => {
+    return knex('log').update({deleted: true}).where({id: rb.id});
+}
+
+const getLogsBetweenDates = (id, today, previousDate) => {
+    return knex('log')
+    .join('language', 'language.id', 'log.language_id')
+    .select(knex.raw("DATE_FORMAT(log.date, '%Y-%m-%d') as date, sum(log.time) as time, language.name as language"))
+    .where({user_id: id})
+    .andWhere('date', '<=', today)
+    .andWhere('date', '>=', previousDate)
+    .andWhere('deleted', '=', '0')
+    .groupBy('date')
+    .groupBy('language_id')
+    .orderBy('language_id')
+    .orderBy('date', 'asc');
+}
+
+const getLastWeek = (rb, ru) => {
+    return getLogsBetweenDates(ru.id, rb.today, rb.lastWeek);
+}
+
+const getLastMonth = (rb, ru) => {
+    return getLogsBetweenDates(ru.id, rb.today, rb.lastMonth);
+}
+
+const getLastYear = (rb, ru) => {
+    return knex('log')
+    .join('language', 'language.id', 'log.language_id')
+    .select(knex.raw('month(date) as date, sum(log.time) as time, language.name as language'))
+    .where({user_id: ru.id})
+    .andWhere('date', '<=', rb.today)
+    .andWhere('date', '>=', rb.lastYear)
+    .andWhere('deleted', '=', '0')
+    .groupBy(knex.raw('month(date)'))
+    .groupBy('language_id')
+    .orderBy('language_id')
+    .orderBy('date', 'asc');
+}
+
+const getAllTime = (rb, ru) => {
+    return knex('log')
+    .join('language', 'language.id', 'log.language_id')
+    .select(knex.raw('year(date) as year, sum(log.time) as time, language.name as language'))
+    .where({user_id: ru.id})
+    .andWhere('date', '<=', rb.today)
+    .andWhere('deleted', '=', '0')
+    .groupBy(knex.raw('year(date)'))
+    .groupBy('language_id')
+    .orderBy('language_id')
+    .orderBy('year', 'asc');
+}
+
+module.exports = {
+    getAllLogsByID,
+    getLogsDateByID,
+    insertLog,
+    deleteLog,
+    getLastWeek,
+    getLastMonth,
+    getLastYear,
+    getAllTime
+}
--- routes/log.routes.js
+++ routes/log.routes.js
@@ -1,39 +1,25 @@
 const router = require('express').Router();
-const knex = require('../config/KnexConnection');
+const logModels = require('../models/log.models');
 
 // get all logs for a user
 router.get('/getAllLogs', async (req, res) => {
-
-    let result = await knex('log').where({user_id: req.user.id, deleted: false}).orderBy('date_created', 'desc');
+    let result = await logModels.getAllLogsByID(req.user.id);
     res.send(result);
 })
 
 router.get('/getLogsDate', async (req, res) => {
-
-    let result = await knex('log').select(knex.raw("distinct(DATE_FORMAT(date_created, '%Y-%m-%d')) as date")).where({user_id: req.user.id, deleted: false}).orderBy('date', 'desc');
+    let result = await logModels.getLogsDateByID(req.user.id);
     res.send(result);
 })
 
 // create a new log
 router.post('/create', async (req, res) => {
-    // create a log
-    // a log has these columns name, time, date, type_id, user_id, language_id
-    let log = {
-        name: req.body.name,
-        user_id: parseInt(req.user.id),
-        language_id: parseInt(req.body.language_id),
-        type_id: parseInt(req.body.type_id),
-        time: req.body.time,
-        date: req.body.date,
-        date_created: new Date().toISOString().slice(0,10),
-    }
-    let result = await knex('log').insert(log);
-    //res.send(result[0].id)
+    await logModels.insertLog(req.body, req.user);
     res.redirect('/')
 })
 
 router.post('/delete', async (req, res) => {
-    let result = await knex('log').update({deleted: true}).where({id: req.body.id});
+    let result = await logModels.deleteLog(req.body);
     let status;
     if (result > 0) {
         status = 200;
@@ -44,71 +30,24 @@ router.post('/delete', async (req, res) => {
 })
 
 router.post('/getLastWeek', async (req, res) => {
-    let result = await knex('log')
-    .join('language', 'language.id', 'log.language_id')
-    .select(knex.raw("DATE_FORMAT(log.date, '%Y-%m-%d') as date, sum(log.time) as time, language.name as language"))
-    .where({user_id: req.user.id})
-    .andWhere('date', '<=', req.body.today)
-    .andWhere('date', '>=', req.body.lastWeek)
-    .andWhere('deleted', '=', '0')
-    .groupBy('date')
-    .groupBy('language_id')
-    .orderBy('language_id')
-    .orderBy('date', 'asc')
-
+    let result = await logModels.getLastWeek(req.body, req.user);
     res.send(result);
 })
 
 router.post('/getLastMonth', async (req, res) => {
 
-    let result = await knex('log')
-    .join('language', 'language.id', 'log.language_id')
-    .select(knex.raw("DATE_FORMAT(log.date, '%Y-%m-%d') as date, sum(log.time) as time, language.name as language"))
-    .where({user_id: req.user.id})
-    .andWhere('date', '<=', req.body.today)
-    .andWhere('date', '>=', req.body.lastMonth)
-    .andWhere('deleted', '=', '0')
-    .groupBy('date')
-    .groupBy('language_id')
-    .orderBy('language_id')
-    .orderBy('date', 'asc')
+    let result = await logModels.getLastMonth(req.body, req.user)
     res.send(result);
 
 })
 
 router.post('/getLastYear', async (req, res) => {
-
-    let date = new Date();
-    
-    let result = await knex('log')
-    .join('language', 'language.id', 'log.language_id')
-    .select(knex.raw('month(date) as date, sum(log.time) as time, language.name as language'))
-    .where({user_id: req.user.id})
-    .andWhere('date', '<=', req.body.today)
-    .andWhere('date', '>=', req.body.lastYear)
-    .andWhere('deleted', '=', '0')
-    .groupBy(knex.raw('month(date)'))
-    .groupBy('language_id')
-    .orderBy('language_id')
-    .orderBy('date', 'asc')
-
+    let result = await logModels.getLastYear(req.body, req.user);
     res.send(result);
 })
 
 router.post('/getAllTime', async (req, res) => {
-
-    
-    let result = await knex('log')
-    .join('language', 'language.id', 'log.language_id')
-    .select(knex.raw('year(date) as year, sum(log.time) as time, language.name as language'))
-    .where({user_id: req.user.id})
-    .andWhere('date', '<=', req.body.today)
-    .andWhere('deleted', '=', '0')
-    .groupBy(knex.raw('year(date)'))
-    .groupBy('language_id')
-    .orderBy('language_id')
-    .orderBy('year', 'asc')
-
+    let result = await logModels.getAllTime(req.body, req.user);
     res.send(result);
 })
 
