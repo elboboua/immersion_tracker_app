894fb7affa8b5d59ed30d703ad7c8f412d3bd110
elboboua <elboboua@oregonstate.edu>
2020-06-30 21:13:50 +0300

    fixed big bug in user logs

--- public/js/user/getLogs.js
+++ public/js/user/getLogs.js
@@ -18,12 +18,6 @@ const getLogs = async () => {
         title.className = 'header-row title'
         title.innerText = result[i].name;
         header.appendChild(title);
-
-        let deleteButton = document.createElement('div');
-        deleteButton.className = 'header-row delete-button';
-        deleteButton.id = 'delete_' + result[i].id;
-        deleteButton.innerHTML = '<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/><path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/></svg>'
-        header.appendChild(deleteButton);
         card.appendChild(header);
 
 
@@ -62,29 +56,6 @@ const getLogs = async () => {
         card.appendChild(body);
         logContainer.appendChild(card)
 
-        // define function for delete button underneath the call to add the delete button to the DOM
-        deleteButton.addEventListener('click', (e) => {
-            let deletedCard = document.getElementById(deleteButton.id.replace('delete', 'card'));
-            let r = confirm('Are you sure you want to delete this log?')
-            if (r) {
-                deletedCard.style.display = 'none';
-
-                let request_body =  {
-                    id : deletedCard.id.replace('card_', '')
-                }
-
-                fetch('/log/delete', {
-                    method: 'post',
-                    headers: {
-                        'Content-Type' : 'application/json'
-                    },
-                    body: JSON.stringify(request_body),
-                })
-            } else {
-                return;
-            }
-            
-        })
     }
     
 }
