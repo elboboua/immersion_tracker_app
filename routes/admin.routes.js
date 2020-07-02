923f37a6b5cd23b70d4e56555c5be7127fa738b9
elboboua <elboboua@oregonstate.edu>
2020-07-02 17:53:02 +0300

    created an admin page to be able to see basic stats

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -16,10 +16,36 @@ router.get('/count-logs', async (req, res) => {
     res.send(result[0]);
 })
 
+router.get('/count-hours', async (req, res) => {
+    let result = await knex('log').select(knex.raw('sum(time) as minutes')).where('deleted', '=', '0');
+    result[0].hours = result[0].minutes/60;
+    res.send(result[0]);
+})
+
 router.get('/count-languages', async (req, res) => {
     let result = await knex('log').select(knex.raw('count(distinct language_id) as count'));
     console.log(result)
     res.send(result[0]);
+})
+
+router.get('/get-top-users', async (req, res) => {
+    let result = await knex('log')
+                .join('user', 'user.id', 'log.user_id')
+                .select(knex.raw('user.username, count(log.user_id) as count'))
+                .groupBy('log.user_id')
+                .where('user.username', '<>', '""')
+                .orderBy('count', 'desc');
+    res.send(result);
+})
+
+router.get('/get-new-users', async (req, res) => {
+    let result = await knex('log')
+                .join('user', 'user.id', 'log.user_id')
+                .select(knex.raw('user.username, count(log.user_id) as count'))
+                .groupBy('log.user_id')
+                .where('user.username', '<>', '""')
+                .orderBy('user.id', 'desc');
+    res.send(result);
 })
 
 module.exports = router;
