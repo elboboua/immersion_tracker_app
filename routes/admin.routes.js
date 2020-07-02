31a16ba066d70b33afa0e46055c758d937fab68d
elboboua <elboboua@oregonstate.edu>
2020-07-02 14:44:37 +0300

    testing ads on master :(

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send({
-        message: 'admin route is up'
-    })
+    res.render('admin')
 })
 
 module.exports = router;
