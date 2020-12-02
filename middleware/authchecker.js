84debb3ea0657fd2060b031a64c284d5aab3ca4f
elboboua <elboboua@oregonstate.edu>
2020-12-02 14:31:39 -0600

    got jwt authorization working

--- middleware/authchecker.js
+++ middleware/authchecker.js
@@ -1,8 +1,20 @@
+const jwt = require('jsonwebtoken')
+
 const isAuthorized = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/auth/login')
+    const token = req.header('auth-token');
+    
+    console.log(token)
+
+    if (!token) return res.status(401).send('access denied')
+
+    try {
+        const verified = jwt.verify(token, 'randomnums')
+        console.log(verified.user)
+        req.user = verified.user;
+        console.log(req.user)
+        next()
+    } catch {
+        res.status(400).send('Invalid Token')
     }
 }
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken')
 
 
 router.get('/login', (req, res) => {
@@ -14,8 +15,11 @@ router.get('/google', passport.authenticate('google',
 );
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    console.log(req)
-    res.redirect('/');
+    console.log(req.user)
+
+    let token = jwt.sign({user: req.user}, 'randomnums')
+
+    res.redirect(`exp://10.0.0.236:19000?authToken=${token}`);
 })
 
 router.get('/logout', (req, res) => {
