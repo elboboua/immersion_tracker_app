2d68089edee39e957336c69ffe57464a827448cd
elboboua <elboboua@oregonstate.edu>
2021-02-12 22:20:41 -0600

    fixing week graph issue

--- public/js/analyticsWeek.homepage.js
+++ public/js/analyticsWeek.homepage.js
@@ -1,76 +1,56 @@
 // get the dates for the request
 
-
 // request the json
 const getLastWeeksLog = async () => {
-    let datesThisWeek = [];
-    for (let i = 0; i < 7; i++) {
-        datesThisWeek.unshift(moment().subtract(i, 'days').format().slice(0,10));
-    }
-    let lastWeekAndToday = {
-        today: moment().format(),
-        lastWeek: moment().subtract(6, 'd').format()
-    }
-    lastWeekAndToday = JSON.stringify(lastWeekAndToday);
-    
-    let result = await fetch('/homepage/getLastWeek', {
-        method: 'post',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: lastWeekAndToday
-    })
-    
-    result = await result.json();
-    
-    
-        
-        let graphObj = {
-            dates: datesThisWeek,
-            cumulativeTimes: new Array(7).fill(0),
-            individualTimes: new Array(7).fill(0),
-        }
-        
-        result.forEach((object) => {
-                
-                graphObj.individualTimes[datesThisWeek.indexOf(object.date.slice(0,10))] = Math.round(object.time/60*100)/100;
-                
-                graphObj.title = 'Hours Logged This Week'
+  let datesThisWeek = [];
+  for (let i = 0; i < 7; i++) {
+    datesThisWeek.unshift(moment().subtract(i, "days").format().slice(0, 10));
+  }
+  let lastWeekAndToday = {
+    today: moment().format(),
+    lastWeek: moment().subtract(7, "d").format(),
+  };
+  lastWeekAndToday = JSON.stringify(lastWeekAndToday);
 
-        })
-        
-        //create cumulativeTimes
-        let sum = 0;
-        for (let i = 0; i< graphObj.cumulativeTimes.length; i++) {
-            sum+= graphObj.individualTimes[i];
-            graphObj.cumulativeTimes[i] = sum;
-        }
-        
-        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        
-        graphObj.dates = graphObj.dates.map((date) => {
-            let day = new Date(date);
-            date = day.getDay().toString();
-            return days[date]
-        })
-        
-        
-        createLanguageGraph(graphObj, 'graph-container', 'Hours per Day')
-        
-    
-}
+  let result = await fetch("/homepage/getLastWeek", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: lastWeekAndToday,
+  });
 
+  result = await result.json();
 
+  let graphObj = {
+    dates: datesThisWeek,
+    cumulativeTimes: new Array(7).fill(0),
+    individualTimes: new Array(7).fill(0),
+  };
 
+  result.forEach((object) => {
+    graphObj.individualTimes[datesThisWeek.indexOf(object.date.slice(0, 10))] =
+      Math.round((object.time / 60) * 100) / 100;
 
+    graphObj.title = "Hours Logged This Week";
+  });
 
+  //create cumulativeTimes
+  let sum = 0;
+  for (let i = 0; i < graphObj.cumulativeTimes.length; i++) {
+    sum += graphObj.individualTimes[i];
+    graphObj.cumulativeTimes[i] = sum;
+  }
 
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-getLastWeeksLog()
+  graphObj.dates = graphObj.dates.map((date) => {
+    let day = new Date(date);
+    date = day.getDay().toString();
+    return days[date];
+  });
 
+  createLanguageGraph(graphObj, "graph-container", "Hours per Day");
+};
 
-
-
-
-
-
+getLastWeeksLog();
--- public/js/user/analyticsWeek.js
+++ public/js/user/analyticsWeek.js
@@ -1,85 +1,68 @@
 // get the dates for the request
 
-
 // request the json
 const getLastWeeksLog = async () => {
-    let datesThisWeek = [];
-    for (let i = 0; i < 7; i++) {
-        datesThisWeek.unshift(moment().subtract(i, 'days').format().slice(0,10));
-    }
-    let lastWeekAndToday = {
-        today: moment().format(),
-        lastWeek: moment().subtract(6, 'd').format()
-    }
-    lastWeekAndToday = JSON.stringify(lastWeekAndToday);
+  let datesThisWeek = [];
+  for (let i = 0; i < 7; i++) {
+    datesThisWeek.unshift(moment().subtract(i, "days").format().slice(0, 10));
+  }
+  let lastWeekAndToday = {
+    today: moment().format(),
+    lastWeek: moment().subtract(7, "d").format(),
+  };
+  lastWeekAndToday = JSON.stringify(lastWeekAndToday);
 
-    let result = await fetch(`/user/${user.username}/getLastWeek`, {
-        method: 'post',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: lastWeekAndToday
-    })
-    
-    result = await result.json();
-    
-    // create an array of languages
-    let languages = [];
+  let result = await fetch(`/user/${user.username}/getLastWeek`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: lastWeekAndToday,
+  });
+
+  result = await result.json();
+
+  // create an array of languages
+  let languages = [];
+  result.forEach((object) => {
+    if (!languages.includes(object.language)) {
+      languages.push(object.language);
+    }
+  });
+
+  languages.forEach((language) => {
+    let graphObj = {
+      dates: datesThisWeek,
+      cumulativeTimes: new Array(7).fill(0),
+      individualTimes: new Array(7).fill(0),
+    };
+
     result.forEach((object) => {
-        if (!languages.includes(object.language)) {
-            languages.push(object.language)
-        }
-    })
-    
-    languages.forEach((language) => {
-        
-        let graphObj = {
-            dates: datesThisWeek,
-            cumulativeTimes: new Array(7).fill(0),
-            individualTimes: new Array(7).fill(0),
-        }
-        
-        result.forEach((object) => {
-            if (object.language == language) {
-                
-                graphObj.individualTimes[datesThisWeek.indexOf(object.date.slice(0,10))] = Math.round(object.time/60*100)/100;
-                
-                graphObj.title = object.language
-            }
-        })
-        
-        //create cumulativeTimes
-        let sum = 0;
-        for (let i = 0; i< graphObj.cumulativeTimes.length; i++) {
-            sum+= graphObj.individualTimes[i];
-            graphObj.cumulativeTimes[i] = sum;
-        }
-        
-        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      if (object.language == language) {
+        graphObj.individualTimes[
+          datesThisWeek.indexOf(object.date.slice(0, 10))
+        ] = Math.round((object.time / 60) * 100) / 100;
 
-        graphObj.dates = graphObj.dates.map((date) => {
-            date = moment(date).format('dddd')
-            return date.slice(0,3)
-        })
-        
-        
-        createLanguageGraph(graphObj, 'graph-container-week', 'Hours per Day')
-        
-    })
-    
-}
+        graphObj.title = object.language;
+      }
+    });
 
+    //create cumulativeTimes
+    let sum = 0;
+    for (let i = 0; i < graphObj.cumulativeTimes.length; i++) {
+      sum += graphObj.individualTimes[i];
+      graphObj.cumulativeTimes[i] = sum;
+    }
 
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+    graphObj.dates = graphObj.dates.map((date) => {
+      date = moment(date).format("dddd");
+      return date.slice(0, 3);
+    });
 
+    createLanguageGraph(graphObj, "graph-container-week", "Hours per Day");
+  });
+};
 
-
-
-getLastWeeksLog()
-
-
-
-
-
-
-
+getLastWeeksLog();
