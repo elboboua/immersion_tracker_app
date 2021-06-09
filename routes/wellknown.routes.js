ee24ea60ba7750e03e94b58e2b5395d8650dd583
elboboua <elboboua@oregonstate.edu>
2021-06-09 16:37:09 +0300

    resolving crashing issue with aasa

--- routes/wellknown.routes.js
+++ routes/wellknown.routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const knex = require("../config/KnexConnection");
+const fs = require("fs");
+const path = require("path");
 
-const aasa = require("../apple-app-site-association.json");
+const aasa = fs.readFileSync(
+  path.resolve(__dirname, "../apple-app-site-association")
+);
 
 router.get("/apple-app-site-association", async (req, res) => {
   res.set("Content-Type", "application/json");
